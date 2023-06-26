243c1931968aed00cd17a67c6a415ba52987a29e
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-06-25 21:19:12 -0300

    ok

--- main.js
+++ main.js
@@ -53,6 +53,140 @@ function tablaMultiplicar(numero3) {
 
 let numero3 = prompt("ingresar numero");
 tablaMultiplicar(numero3);
+
+//numeros primos
+
+
+function esPrimo(numero4) {
+    if (numero4 <= 1) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(numero4); i++) {
+      if (numero4 % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  //Ejemplo de uso
+  console.log(esPrimo(7)); 
+  console.log(esPrimo(10)); 
+  
+  //numeros perfectos
+
+  function esNumeroPerfecto(numero){
+    if (numero <= 1) {
+        return false;
+        
+    } 
+
+    let sumaDivisores= 0;
+
+    for (let i = 1; i < numero; i++){
+        if (numero % i === 0){
+            sumaDivisores +=  i;
+        }
+    }
+    return sumaDivisores === numero;
+        
+    }
+    //ejemplo de uso
+    console.log(esNumeroPerfecto(28));
+    console.log(esNumeroPerfecto(12));
+
+    //palindromo
+
+
+    function esPalindromo(cadena){
+        let array = cadena.split("");
+        let reverse = array.reverse();
+        return cadena ==reverse.join("") ? "si es" :"no es"
+    }
+    //ejemplp 
+    
+    console.log(esPalindromo("oso"));
+    console.log(esPalindromo("revolver"));
+    console.log(esPalindromo("amor"));
+        
+   //calculo del factorial
+   //5!=1*2*3*4*5=120
+   // 1!=1
+   // 0!=1 
+    
+   function factorialRecursivo(n){
+    if (n==0){
+        return 1;
+    }else {
+        return n*factorialRecursivo(n-1);
+    }
+   }
+   console.log(factorialRecursivo(5));
+/*
+   //ordenar palbras
+
+   function ordenarPalabras() {
+    let ol = document.getElementById("lista-palabras");
+    ol.innerHTML = "";
+    let cadena = document.getElementById("text1").value;
+    let palabrasOrdenadas = cadena.trim().split(" ");
+    palabrasOrdenadas.sort();
+    for (let i = 0; i < palabrasOrdenadas.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = palabrasOrdenadas[i];
+        ol.appendChild(li);
+    }
+    document.getElementById("text1").select();
+}
+
+function reset() {
+    document.getElementById("text1").value = "";
+    document.getElementById("text1").focus();
+    document.getElementById("lista-palabras").innerHTML = "";
+}
+
+//esta comentado todo este ejercicico,porque al momento de ordenar no funciona.no toma el elemento (li.innerHTML)a la palabra ordenada (I)
+
+*/
+   
+
+
+//contar letras
+
+function contarVocales(frase) {
+    if (typeof frase !== 'string') {
+      throw new TypeError('El argumento debe ser una cadena de caracteres');
+    }
+  
+    let vocales = 'aeiouAEIOU';
+    let contadorVocales = 0;
+  
+    for (let i = 0; i < frase.length; i++) {
+      if (vocales.indexOf(frase[i]) !== -1) {
+        contadorVocales++;
+      }
+    }
+  
+    return contadorVocales;
+  }
+  
+  try {
+    console.log(contarVocales("javascript es genial"));
+  } catch (e) {
+    console.log(`Error: ${e.message}`);
+  }
+  
+//ordenar palabras
+
+function ordenarTexto(texto) {
+    return [...texto].sort((a, b) => a.localeCompare(b)).join("");
+  }
+  
+  console.log(ordenarTexto("javascript"));
+  
+  
     
 
     
